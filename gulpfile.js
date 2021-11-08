998ad6f8efc5973700c3a205a001840cc1649c7f
ytng1095 <nevinngyt@yahoo.com.sg>
2021-11-09 01:37:07 +0800

    Gulp 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
     browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
@@ -10,28 +18,65 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
     });
     
-    gulp.task('sass:watch', function () {
-        gulp.watch('./css/*.scss', ['sass']);
+gulp.task('sass:watch', function () {
+    gulp.watch('./css/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', function () {
+    var files = [
+    './*.html',
+    './css/*.css',
+    './img/*.{png,jpg,gif}',
+    './js/*.js'
+    ];
+
+        
+    
+browserSync.init(files, {
+    server: {
+        baseDir: "./"
+    }
     });
+
+});
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+        return stream
+            .pipe(usemin({
+                css: [ rev() ],
+                html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+                js: [ uglify(), rev() ],
+                inlinejs: [ uglify() ],
+                inlinecss: [ cleanCss(), 'concat' ]
+            }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
     
-    gulp.task('browser-sync', function () {
-        var files = [
-        './*.html',
-        './css/*.css',
-        './img/*.{png,jpg,gif}',
-        './js/*.js'
-        ];
-    
-    browserSync.init(files, {
-        server: {
-            baseDir: "./"
-        }
-        });
-    
-    });
-    
-    // Default task
-    gulp.task('default', ['browser-sync'], function() {
-        gulp.start('sass:watch');
-    });
-    
+gulp.task('build',['clean'], function() {
+        gulp.start('copyfonts','imagemin','usemin');
+});
+
+// Default task
+gulp.task('default', ['browser-sync'], function() {
+    gulp.start('sass:watch');
+});
